a4cee479746366d1a9bb72f8a37bc0f6e87c7add
Yuewei Li <42328655+404Toncho@users.noreply.github.com>
2025-06-20 23:40:40 +0100

    Add and refine Home page Hero section
    * Add home page hero section
    * Update hero section layout
    * Refine hero section for responsive design

---
+++ src/app/home/page.tsx
@@ -0,0 +1,14 @@
+import HeroSection from "@/components/home/hero/HeroSection";
+
+export default function HomePage() {
+    return (
+        <main className=" ">
+            <HeroSection />
+            {/* TODO:  AboutSection */}
+            {/* TODO:  TechStackSection */}
+            {/* TODO:  ExperienceSection */}
+            {/* TODO:  ProjectsSection */}
+            {/* TODO:  ContactSection */}
+        </main>
+    );
+}
